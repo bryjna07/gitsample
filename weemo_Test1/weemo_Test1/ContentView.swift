660305b8bb0f4e2159e2aaec749fbd2536439b9e
Reimos7 <tjrvud9@gmail.com>
2025-11-07 14:55:59 +0900

    test

---
+++ weemo_Test1/weemo_Test1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weemo_Test1
+//
+//  Created by YoungJin on 11/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weemo_Test1/weemo_Test1/weemo_Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  weemo_Test1App.swift
+//  weemo_Test1
+//
+//  Created by YoungJin on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct weemo_Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
